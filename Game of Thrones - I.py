f426c7439c165534ace5591f8e9854f66dd5eceb
Nishant Kumar <nishant.kumar9500@gmail.com>
2018-11-15 00:49:21 -0500

    Update Game of Thrones - I.py

--- Game of Thrones - I.py
+++ Game of Thrones - I.py
@@ -7,28 +7,10 @@ import re
 import sys
 
 def gameOfThrones(s):
-    s2={}
-    n=s
-    j=1
     m=0
-    c=[1]*len(s)
-    for i in range(len(s)-1):
-        j=0
-        while j<len(s):
-            if j==i:
-                j=j+1
-            if(n[i]==n[j]):
-                c[i]=c[i]+1
-                j=j+1
-            else: 
-                j=j+1
-    
-    for i in range(len(c)):
-        if c[i]%2==0:
-            m=m+1
-        elif c[i]>2 and c[i]%2==1:
-            m=m+1
-    if m==len(c) or m==len(c)-1:
+    for x in set(s):
+        m=m+(s.count(x)%2)
+    if m<2:
         return "YES"
     else:
         return "NO"
